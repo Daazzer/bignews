beeab832d12646cd0b9ca123a2b52898bace30da
Daazzer <Daazzer@github.com>
2020-09-29 09:44:27 +0800

    实现修改用户信息模块

--- admin/js/user.js
+++ admin/js/user.js
@@ -1,20 +1,62 @@
-$.ajax({
-  url: window.BigNews.user_detail,
-  dataType: 'json',
-  success(res) {
-    console.log(res);
-    if (res.code === 200) {
-      const userData = res.data;
-      // const userPic = userData.userPic;
-      for (const key in userData) {
-        if (key === 'userPic') {
-          $(`#${key}`).prop('src', userData[key]);
-        } else {
-          $(`#${key}`).val(userData[key]);
+(() => {
+  $.ajax({
+    url: window.BigNews.user_detail,
+    dataType: 'json',
+    success(res) {
+      if (res.code === 200) {
+        const userData = res.data;
+        // const userPic = userData.userPic;
+        for (const key in userData) {
+          if (key === 'userPic') {
+            $('#userPicImg').prop('src', userData[key]);
+          } else {
+            $(`#${key}`).val(userData[key]);
+          }
+        }
+      } else {
+        alert(res.msg);
+      }
+    }
+  });
+
+  $('#editBtn').click(e => {
+    e.preventDefault();
+    const userEditForm = $('#form').get(0);
+    const userEditFd = new FormData(userEditForm);
+
+    $.ajax({
+      type: 'post',
+      url: window.BigNews.user_edit,
+      data: userEditFd,
+      contentType: false,
+      processData: false,
+      dataType: 'json',
+      success(res) {
+        $('#userModal').modal('show');
+        $('#userModal .modal-tips').text(res.msg);
+        if (res.code === 200) {
+          $('#userModal').on('hide.bs.modal', () => {
+            // 在 index 页面发请求重新渲染页面
+            const indexPage = window.parent;
+            // 渲染页面
+            indexPage.$.ajax({
+              url: window.BigNews.user_info,
+              dataType: 'json',
+              success(res) {
+                if (res.code === 200) {
+                  indexPage.$('#nickname').text(res.data.nickname);
+                  indexPage.$('img.user_avatar').prop('src', res.data.userPic);
+                }
+              }
+            });
+          });
         }
       }
-    } else {
-      alert(res.msg);
-    }
-  }
-});
+    });
+  });
+
+  $('#userPic').change(e => {
+    const imgSrc = URL.createObjectURL(e.target.files[0]);
+    $('#userPicImg').prop('src', imgSrc);
+  });
+})();
