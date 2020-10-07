a521737b96f0b22b0d1971b291d8b4023c8c2783
Daazzer <Daazzer@github.com>
2020-10-07 13:00:04 +0800

    实现用户首页展示模块

---
+++ components/commentList.js
@@ -0,0 +1,120 @@
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function commentList(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {"commentList.pug":"each comment in comments\r\n  li\r\n    |\r\n    |\r\n    span= comment.author.substr(0, 1)\r\n    |\r\n    |\r\n    b.\r\n      #[em= comment.author]\r\n      #{comment.date}说:\r\n    |\r\n    |\r\n    strong= comment.intro\r\n    |\r\n    |"};
+;var locals_for_with = (locals || {});(function (comments) {;pug_debug_line = 1;pug_debug_filename = "commentList.pug";
+// iterate comments
+;(function(){
+  var $$obj = comments;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index0 = 0, $$l = $$obj.length; pug_index0 < $$l; pug_index0++) {
+        var comment = $$obj[pug_index0];
+;pug_debug_line = 2;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cli\u003E";
+;pug_debug_line = 3;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 4;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 4;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 5;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cspan\u003E";
+;pug_debug_line = 5;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.author.substr(0, 1)) ? "" : pug_interp)) + "\u003C\u002Fspan\u003E";
+;pug_debug_line = 6;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 7;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 7;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 8;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cb\u003E";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cem\u003E";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.author) ? "" : pug_interp)) + "\u003C\u002Fem\u003E";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 10;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 10;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.date) ? "" : pug_interp));
+;pug_debug_line = 10;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "说:\u003C\u002Fb\u003E";
+;pug_debug_line = 11;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 12;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 12;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 13;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cstrong\u003E";
+;pug_debug_line = 13;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.intro) ? "" : pug_interp)) + "\u003C\u002Fstrong\u003E";
+;pug_debug_line = 14;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 15;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 15;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003C\u002Fli\u003E";
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index0 in $$obj) {
+      $$l++;
+      var comment = $$obj[pug_index0];
+;pug_debug_line = 2;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cli\u003E";
+;pug_debug_line = 3;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 4;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 4;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 5;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cspan\u003E";
+;pug_debug_line = 5;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.author.substr(0, 1)) ? "" : pug_interp)) + "\u003C\u002Fspan\u003E";
+;pug_debug_line = 6;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 7;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 7;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 8;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cb\u003E";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cem\u003E";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.author) ? "" : pug_interp)) + "\u003C\u002Fem\u003E";
+;pug_debug_line = 9;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 10;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 10;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.date) ? "" : pug_interp));
+;pug_debug_line = 10;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "说:\u003C\u002Fb\u003E";
+;pug_debug_line = 11;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 12;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 12;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 13;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003Cstrong\u003E";
+;pug_debug_line = 13;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = comment.intro) ? "" : pug_interp)) + "\u003C\u002Fstrong\u003E";
+;pug_debug_line = 14;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "";
+;pug_debug_line = 15;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\n";
+;pug_debug_line = 15;pug_debug_filename = "commentList.pug";
+pug_html = pug_html + "\u003C\u002Fli\u003E";
+    }
+  }
+}).call(this);
+}.call(this,"comments" in locals_for_with?locals_for_with.comments:typeof comments!=="undefined"?comments:undefined));} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line, pug_debug_sources[pug_debug_filename]);};return pug_html;}
---
+++ js/index.js
@@ -0,0 +1,69 @@
+(() => {
+  // 渲染热门焦点
+  $.ajax({
+    url: window.bignews.index_hotpic,
+    success(res) {
+      if (res.code === 200) {
+        const focusNewsHTML = window.focusNews({ pictures: res.data });
+        $('.focus_list').html(focusNewsHTML);
+      }
+    }
+  });
+
+  // 渲染分类列表
+  $.ajax({
+    url: window.bignews.index_category,
+    success(res) {
+      if (res.code === 200) {
+        const categoryListHTML = window.categoryList({ categoryItems: res.data });
+        $('.level_two').html(categoryListHTML);
+      }
+    }
+  });
+
+  // 渲染最新资讯
+  $.ajax({
+    url: window.bignews.index_latest,
+    success(res) {
+      if (res.code === 200) {
+        // 返回的富文本编辑器的正文内容，如果模板引擎转义则会发生标签出现在内容中的情况，如果不转义则会发生html中出现多余标签的情况
+        const commonNewsListHTML = window.commonNewsList({ commonNewsItems: res.data });
+        $('.common_news').html(commonNewsListHTML);
+      }
+    }
+  });
+
+  // 渲染热门排行
+  $.ajax({
+    url: window.bignews.index_rank,
+    success(res) {
+      if (res.code === 200) {
+        const hotrankListHTML = window.hotrankList({ hotrankItems: res.data });
+        $('.hotrank_list').html(hotrankListHTML);
+      }
+    }
+  });
+
+  // 渲染最新评论
+  $.ajax({
+    url: window.bignews.index_latest_comment,
+    success(res) {
+      if (res.code === 200) {
+        const commentListHTML = window.commentList({ comments: res.data });
+        $('.comment_list').html(commentListHTML);
+      }
+    }
+  });
+
+  // 渲染焦点关注
+  $.ajax({
+    url: window.bignews.index_attention,
+    success(res) {
+      console.log(res);
+      if (res.code === 200) {
+        const attentionListHTML = window.attentionList({ attentionItems: res.data });
+        $('.guanzhu_list').html(attentionListHTML);
+      }
+    }
+  });
+})();
